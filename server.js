4c6a4a1ba3e1db15ea44fa271ae4cd6ed0ebc02b
Viktor Solovyev <barn-mih@rambler.ru>
2023-02-10 02:06:01 +0300

    feat: init db with start server

--- server.js
+++ server.js
@@ -4,14 +4,19 @@ import cors from "cors";
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:8081"
+const corsOptions = {
+  origin: "http://localhost:8081",
 };
 
 app.use(cors(corsOptions));
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and Resync Db");
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to rs-clone-twitter-api application." });
