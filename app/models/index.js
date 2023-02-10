4d0dd6cc78f62e27721844c5d60ff64df4c88566
Viktor Solovyev <barn-mih@rambler.ru>
2023-02-10 15:10:09 +0300

    Update index.js

--- app/models/index.js
+++ app/models/index.js
@@ -2,7 +2,7 @@ const config = require("../config/db.config.js");
 const initUser = require("../models/user.model.js");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("postgres:fHlx86Zyjvew7K3sRjI2@containers-us-west-97.railway.app:6487/railway");
+const sequelize = new Sequelize("postgresql://postgres:fHlx86Zyjvew7K3sRjI2@containers-us-west-97.railway.app:6487/railway");
 
 const db = {};
 
