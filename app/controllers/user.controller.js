6a08cab9c190d437764bdb8b51490f5f75e0149d
Viktor Solovyev <barn-mih@rambler.ru>
2023-02-13 14:08:32 +0300

    feat: add followers info to user api

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,31 +1,43 @@
 const db = require("../models");
 const User = db.user;
+const Follower = db.follower;
 
-exports.getUser = (req, res) => {
+exports.getUser = async (req, res) => {
   const username = req.params.username;
-  User.findOne({
-    where: {
-      username: username,
-    },
-  })
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send({ message: "User Not found." });
-      }
-      res.status(200).send({
-        username: user.username,
-        email: user.email,
-        name: user.name,
-        about: user.about,
-        location: user.location,
-        site: user.site,
-        birthday: user.birthday,
-        registration_date: user.createdAt,
-      });
-    })
-    .catch((err) => {
-      res.status(500).send({ message: err.message });
+  try {
+    const user = await User.findOne({
+      where: {
+        username: username,
+      },
     });
+    if (!user) {
+      return res.status(404).send({ message: "User Not found." });
+    }
+    const amountFollowers = await Follower.count({
+      where: {
+        leader: username,
+      },
+    });
+    const amountFollowing = await Follower.count({
+      where: {
+        follower: username,
+      },
+    });
+    res.status(200).send({
+      username: user.username,
+      email: user.email,
+      name: user.name,
+      about: user.about,
+      location: user.location,
+      site: user.site,
+      birthday: user.birthday,
+      registration_date: user.createdAt,
+      followers: amountFollowers,
+      following: amountFollowing,
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 };
 
 exports.update = (req, res) => {
