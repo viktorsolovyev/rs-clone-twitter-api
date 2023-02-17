ad4b3318e29b3894bcfe9e09acf30dd9e230867b
Viktor Solovyev <barn-mih@rambler.ru>
2023-02-17 23:13:58 +0300

    feat: add full user profile to signin api

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,10 +1,9 @@
 const db = require("../models");
 const config = require("../config/auth.config");
+const userController = require("../controllers/user.controller");
 const User = db.user;
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-
-const Op = db.Sequelize.Op;
 
 exports.signup = (req, res) => {
   // save User to Database
@@ -19,54 +18,47 @@ exports.signup = (req, res) => {
     birthday: req.body.birthday,
   })
     .then((user) => {
-        res.status(200).send({ message: "User was registered successfully!" });
+      res.status(200).send({ message: "User was registered successfully!" });
     })
     .catch((err) => {
       res.status(500).send({ message: err.message });
     });
 };
 
-exports.signin = (req, res) => {
-  User.findOne({
-    where: {
-      username: req.body.username,
-    },
-  })
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send({ message: "User Not found." });
-      }
-
-      const passwordIsValid = bcrypt.compareSync(
-        req.body.password,
-        user.password
-      );
-
-      if (!passwordIsValid) {
-        return res.status(401).send({
-          accessToken: null,
-          message: "Invalid Password!",
-        });
-      }
-
-      const token = jwt.sign({ id: user.id }, config.secret, {
-        expiresIn: 86400, // 24 hours
-      });
-
-      res.status(200).send({
-        id: user.id,
-        username: user.username,
-        email: user.email,
-        name: user.name,
-        about: user.about,
-        location: user.location,
-        site: user.site,
-        birthday: user.birthday,
-        registration_date: user.createdAt,
-        accessToken: token,
-      });
-    })
-    .catch((err) => {
-      res.status(500).send({ message: err.message });
+exports.signin = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
     });
+
+    if (!user) {
+      return res.status(404).send({ message: "User Not found." });
+    }
+
+    const passwordIsValid = bcrypt.compareSync(
+      req.body.password,
+      user.password
+    );
+
+    if (!passwordIsValid) {
+      return res.status(401).send({
+        accessToken: null,
+        message: "Invalid Password!",
+      });
+    }
+
+    const token = jwt.sign({ id: user.id }, config.secret, {
+      expiresIn: 86400, // 24 hours
+    });
+
+    const userProfile = await userController.getUserProfileByUsername(
+      user.username
+    );
+    userProfile.accessToken = token;
+    res.status(200).send(userProfile);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 };
