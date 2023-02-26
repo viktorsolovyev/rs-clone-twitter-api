2aa3d05c505807a86bad66ef5e92173c5d3fca24
Viktor Solovyev <barn-mih@rambler.ru>
2023-02-27 00:51:37 +0300

    Update index.js

--- app/models/index.js
+++ app/models/index.js
@@ -7,7 +7,7 @@ const initLike = require("../models/like.model");
 const initView = require("../models/view.model");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("postgres://root:a4pDPjMC2iYmw9jqJsKZ9hppgacKRvDH@dpg-cftstt94reb6ks1nkar0-a.frankfurt-postgres.render.com/tweetter");
+const sequelize = new Sequelize("postgres://root:a4pDPjMC2iYmw9jqJsKZ9hppgacKRvDH@dpg-cftstt94reb6ks1nkar0-a/tweetter");
 
 const db = {};
 
